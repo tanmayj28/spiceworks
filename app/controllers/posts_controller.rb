8b39446d3129f129cafc08a66e8386d98a1242f2
7anminator <tanmay@happyness.net>
2020-01-04 17:25:34 +0530

    adds post controller with basic operations

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.fetch(:post, {}).permit!
+    end
+end
